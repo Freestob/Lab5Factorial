4a163c0510cd1a97744bdea68b54e047f9083a93
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-19 14:17:17 -0400

    First commit.

---
+++ Lab5Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number.");
+            int userNumber = int.Parse(Console.ReadLine());
+            int x = userNumber;
+            for (int i = 1; i < x; i++)
+            {
+                int factorial = userNumber * --userNumber;
+                Console.WriteLine(factorial);
+                Console.ReadKey();
+            }
+            
+
+        }
+
+    }
+}
